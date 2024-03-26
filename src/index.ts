81c3b70b3b85925a27a520ef215eaaf219f300c2
Osama Sayah <osama.sayah123@gmail.com>
2024-03-26 05:24:24 +0200

    first commit

---
+++ src/index.ts
@@ -0,0 +1,119 @@
+
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+
+import {
+    loginRoute,
+    logoutRoute,
+    makeReq,
+    signupRoute,
+    usernameRoute,
+} from './routes.js';
+
+import {
+    LOGIN_PATH,
+    LOGOUT_PATH,
+    SIGNUP_PATH,
+    USERNAME_PATH,
+} from './const.js';
+
+dotenv.config();
+
+const port = process.env.PORT;
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(cors({
+    origin: ['http://localhost:5173','https://osama-sayah.github.io', '*'], // Allow access from any origin
+    credentials: true, // Enable credentials (cookies, authorization headers, etc.)
+}));
+
+//post
+app.post(LOGIN_PATH, loginRoute);
+app.post(LOGOUT_PATH, logoutRoute);
+app.post(SIGNUP_PATH, signupRoute);
+app.post('/api/user/orders/:Id', (req, res) => {
+    const url = req.originalUrl;
+    makeReq(req, res, "POST",`https://final-project-users.onrender.com${url}`);
+});
+app.post('/api/event/comments/:eventId', (req, res) => {
+    const url = req.originalUrl;
+    makeReq(req, res, "POST",`https://final-project-events.onrender.com${url}`);
+});
+app.post('/api/event', (req, res) => {
+    const url = req.originalUrl;
+    makeReq(req, res, "POST",`https://final-project-events.onrender.com${url}`);
+});
+
+//get
+app.get(USERNAME_PATH, usernameRoute);
+app.get('/api/user/orders/:Id', (req, res) => {
+    const url = req.originalUrl;
+    makeReq(req, res, "GET",`https://final-project-users.onrender.com${url}`);
+});
+app.get('/api/user/:userIdOrName', (req, res) => {
+    const url = req.originalUrl;
+    makeReq(req, res, "GET",`https://final-project-users.onrender.com${url}`);
+});
+app.get('/api/event/tickets/price/:eventId', (req, res) => {
+    const url = req.originalUrl;
+    makeReq(req, res, "GET",`https://final-project-events.onrender.com${url}`);
+});
+app.get('/api/event/tickets/amount/:eventId', (req, res) => {
+    const url = req.originalUrl;
+    makeReq(req, res, "GET",`https://final-project-events.onrender.com${url}`);
+});
+app.get('/api/event/date', (req, res) => {
+    const url = req.originalUrl;
+    makeReq(req, res, "GET",`https://final-project-events.onrender.com${url}`);
+});
+app.get('/api/event/comments/amount/:eventId', (req, res) => {
+    const url = req.originalUrl;
+    makeReq(req, res, "GET",`https://final-project-events.onrender.com${url}`);
+});
+app.get('/api/event/comments/:eventId', (req, res) => {
+    const url = req.originalUrl;
+    makeReq(req, res, "GET",`https://final-project-events.onrender.com${url}`);
+});
+app.get('/api/event/available', (req, res) => {
+    const url = req.originalUrl;
+    makeReq(req, res, "GET",`https://final-project-events.onrender.com${url}`);
+});
+app.get('/api/event/:eventId', (req, res) => {
+    const url = req.originalUrl;
+    makeReq(req, res, "GET",`https://final-project-events.onrender.com${url}`);
+});
+app.get('/api/event', (req, res) => {
+    const url = req.originalUrl;
+    makeReq(req, res, "GET",`https://final-project-events.onrender.com${url}`);
+});
+
+//delete
+app.delete('/api/user/orders/:Id', (req, res) => {
+    const url = req.originalUrl;
+    makeReq(req, res, "DELETE",`https://final-project-users.onrender.com${url}`);
+});
+
+//put
+app.put('/api/event/tickets/:eventId', (req, res) => {
+    const url = req.originalUrl;
+    makeReq(req, res, "PUT",`https://final-project-events.onrender.com${url}`);
+});
+app.put('/api/event/:eventId', (req, res) => {
+    const url = req.originalUrl;
+    makeReq(req, res, "PUT",`https://final-project-events.onrender.com${url}`);
+});
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Server running! port ${port}`);
+});
---
+++ src/routes.ts
@@ -0,0 +1,166 @@
+
+import { Request, Response } from 'express';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import axios from 'axios';
+import User from './models/user.js';
+
+
+export const protectedRout = (req: Request, res: Response) => {
+  const token = req.cookies.token;
+  if (!token) {
+    res.status(401).send('Not logged in');
+    return "ERROR";
+  }
+
+  // Verify JWT token
+  let username;
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    username = (payload as JwtPayload).username;
+  }
+  catch (e) {
+    res.status(401).send('Invalid token');
+    return "ERROR";
+  }
+
+  // We are good!
+  return username;
+};
+
+export async function loginRoute(req: Request, res: Response) {
+  const credentials = req.body;
+  try {
+    await User.validate(credentials);
+  }
+  catch (e) {
+    res.status(400).send('Invalid credentials');
+    return;
+  }
+
+  let user;
+
+  try {
+    user = await axios.get(`https://final-project-users.onrender.com/api/user/${credentials.username}`);
+    user = user.data;
+  }catch (error) {
+    if (error.response) {
+      // If the error has a response, it means it's an error response from the server
+      const { status, data } = error.response;
+      res.status(status).send(data);
+    } else {
+      // If the error doesn't have a response, it's likely a network error or something similar
+      res.status(500).send('Internal server error');
+    }
+    return;
+  }
+  if (!user || !await bcrypt.compare(credentials.password, user.password)) {
+    res.status(401).send('Invalid credentials');
+    return;
+  }
+
+  const token = jwt.sign({ username: user.username }, process.env.JWT_SECRET, { expiresIn: '2d' });
+
+  const secure = process.env.NODE_ENV === 'production';
+  //res.cookie('token', token, { httpOnly: true, secure: true, sameSite: 'strict', maxAge: 2 * 24 * 60 * 60 * 1000 }); // Set the token cookie
+  res.cookie('token', token, { httpOnly: true, maxAge: 2 * 24 * 60 * 60 * 1000 }); // Set the token cookie
+  res.status(200).send('Logged in');
+}
+
+export async function logoutRoute(req: Request, res: Response) {
+  const secure = process.env.NODE_ENV === 'production';
+  //res.clearCookie('token', {httpOnly: true, secure: secure, sameSite: 'strict', maxAge: 2 * 24 * 60 * 60 * 1000 }); // Clear the token cookie
+  res.clearCookie('token', {httpOnly: true, maxAge: 2 * 24 * 60 * 60 * 1000 }); // Clear the token cookie
+  res.status(200).send('Logged out');
+}
+
+export async function signupRoute(req: Request, res: Response) {
+  const user = new User(req.body);
+  try {
+    const error = await user.validate();
+  }
+  catch (e) {
+    res.status(400).send('Invalid credentials');
+    return;
+  }
+
+  let response;
+  try {
+    response = await axios.post('https://final-project-users.onrender.com/api/user', req.body);
+  }catch (error) {
+    if (error.response) {
+      // If the error has a response, it means it's an error response from the server
+      const { status, data } = error.response;
+      res.status(status).send(data);
+    } else {
+      // If the error doesn't have a response, it's likely a network error or something similar
+      res.status(500).send('Internal server error');
+    }
+    return;
+  }
+
+  // Access the response status
+  const responseStatus = response.status;
+  res.status(responseStatus).send('User created');
+}
+
+export async function usernameRoute(req: Request, res: Response) {
+  const token = req.cookies.token;
+  if (!token) {
+    res.status(401).send('Not logged in');
+    return;
+  }
+
+  let username;
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    username = (payload as JwtPayload).username;
+  }
+  catch (e) {
+    res.status(401).send('Invalid token');
+    return;
+  }
+
+  res.status(200).send({username});
+}
+
+export async function makeReq(req: Request, res: Response, method: string, url: string) {
+  const username = protectedRout(req,res);
+  if(username == "ERROR"){
+    return;
+  }
+
+  let response;
+  try {
+    switch (method) {
+      case 'GET':
+        response = await axios.get(url, {data: req.body, headers: {'Content-Type': 'application/json'}});
+        res.status(response.status).send(response.data);
+        return;
+      case 'DELETE':
+        response = await axios.delete(url, {data: req.body, headers: {'Content-Type': 'application/json'}});
+        res.status(response.status).send(response.data);
+        return;
+      case 'PUT':
+        response = await axios.put(url, req.body);
+        res.status(response.status).send(response.data);
+        return;
+      case 'POST':
+        response = await axios.post(url, req.body);
+        res.status(response.status).send(response.data);
+        return;
+      default:
+        throw new Error('Invalid HTTP method');
+    }
+  }catch (error) {
+    if (error.response) {
+      // If the error has a response, it means it's an error response from the server
+      const { status, data } = error.response;
+      res.status(status).send(data);
+    } else {
+      // If the error doesn't have a response, it's likely a network error or something similar
+      res.status(500).send('Internal server error');
+    }
+    return;
+  }
+}
