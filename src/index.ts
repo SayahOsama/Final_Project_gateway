ee3cb9a9175eaaa0bfa1c9717a2693b2518d52bf
osama-sayah <157955955+osama-sayah@users.noreply.github.com>
2024-03-27 02:07:06 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -30,7 +30,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin: ['http://localhost:5173','https://osama-sayah.github.io', '*'], // Allow access from any origin
+    origin: ['localhost:5173', 'http://localhost:5173','https://osama-sayah.github.io', '*'], // Allow access from any origin
     credentials: true, // Enable credentials (cookies, authorization headers, etc.)
 }));
 
