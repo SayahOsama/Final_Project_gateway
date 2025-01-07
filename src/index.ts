e2dfaf8572e06eb220c130d0452589947ebcbb2c
SayahOsama <132453413+SayahOsama@users.noreply.github.com>
2025-01-07 18:47:47 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -36,7 +36,7 @@ app.use(cookieParser());
 // })); 
 
 app.use(cors({
-    origin: ['*','https://SayahOsama.github.io','https://SayahOsama.github.io/Final_Project_App'], // Allow access from any origin
+    origin: ['*','https://github.com/SayahOsama','https://github.com/SayahOsama/Final_Project_App'], // Allow access from any origin
     credentials: true, // Enable credentials (cookies, authorization headers, etc.)
 }));
 
